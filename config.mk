e3cc7e4af36efdc140941ac691cbe2b574024b2a
HeroBuxx <me@buxxed.tk>
2023-02-11 20:36:09 +0000

    halcyonui: Refactor for Halcyon
    Signed-off-by: HeroBuxx <me@buxxed.tk>

--- config.mk
+++ config.mk
@@ -19,24 +19,24 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 
 # Bootanimation
 ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
-     PRODUCT_COPY_FILES += vendor/conquerui/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/halcyonui/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
-     PRODUCT_COPY_FILES += vendor/conquerui/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/halcyonui/bootanimation/bootanimation_1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-     PRODUCT_COPY_FILES += vendor/conquerui/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+     PRODUCT_COPY_FILES += vendor/halcyonui/bootanimation/bootanimation_720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else
     ifeq ($(TARGET_BOOT_ANIMATION_RES),)
-        $(warning "conquerUI: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
+        $(warning "halcyonUI: TARGET_BOOT_ANIMATION_RES is undefined, assuming 1080p")
     else
-        $(warning "conquerUI: Current bootanimation res is not supported, forcing 1080p")
+        $(warning "halcyonUI: Current bootanimation res is not supported, forcing 1080p")
     endif
-    PRODUCT_COPY_FILES += vendor/conquerui/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+    PRODUCT_COPY_FILES += vendor/halcyonui/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
 
 # Overlays
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/conquerui/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/halcyonui/overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    vendor/conquerui/overlay/common/
+    vendor/halcyonui/overlay/common/
 
 PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay
