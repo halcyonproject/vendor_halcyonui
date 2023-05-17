00106e5c4d08c0f681945166c8bb1ccf8bc8f8cc
HeroBuxx <me@herobuxx.me>
2023-05-17 07:22:45 +0000

    halcyonui: Clean up non-exist packages and build Theme Picker
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config.mk
+++ config.mk
@@ -46,55 +46,6 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
-# Xtras - fonts
-# arbutus-slab family
+# Theme Picker
 PRODUCT_PACKAGES += \
-    ArbutusSlab-Regular.ttf
-
-# Lustria family
-PRODUCT_PACKAGES += \
-    Lustria-Regular.ttf
-
-# lato family
-PRODUCT_PACKAGES += \
-    Lato-BoldItalic.ttf \
-    Lato-Bold.ttf \
-    Lato-Italic.ttf \
-    Lato-MediumItalic.ttf \
-    Lato-Medium.ttf \
-    Lato-Regular.ttf
-
-# rubik family
-PRODUCT_PACKAGES += \
-    Rubik-BoldItalic.ttf \
-    Rubik-Bold.ttf \
-    Rubik-Italic.ttf \
-    Rubik-MediumItalic.ttf \
-    Rubik-Medium.ttf \
-    Rubik-Regular.ttf
-
-# zilla-slab family
-PRODUCT_PACKAGES += \
-    ZillaSlab-MediumItalic.ttf \
-    ZillaSlab-Medium.ttf \
-    ZillaSlab-SemiBoldItalic.ttf \
-    ZillaSlab-SemiBold.ttf
-
-# Karla family
-PRODUCT_PACKAGES += \
-    Karla-Regular.ttf
-
-# Fraunces family
-PRODUCT_PACKAGES += \
-    Fraunces-Regular.ttf \
-    Fraunces-SemiBold.ttf
-
-# BigShouldersText family
-PRODUCT_PACKAGES += \
-    BigShouldersText-Bold.ttf \
-    BigShouldersText-ExtraBold.ttf
-
-# Barlow family
-PRODUCT_PACKAGES += \
-    Barlow-Bold.ttf \
-    Barlow-Medium.ttf
+    ThemePicker
