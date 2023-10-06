e81164a336cf2de87cbbb6b4b1cb83c492abd207
HeroBuxx <me@herobuxx.me>
2023-10-06 20:25:52 +0000

    [DNM] halcyonUI: Remove packages and overlay
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config.mk
+++ config.mk
@@ -20,98 +20,20 @@ include $(call all-subdir-makefiles,$(LOCAL_PATH))
 # Boot Animation
 $(call inherit-product, vendor/halcyonui/bootanimation/bootanimation.mk)
 
-# Fonts
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/halcyonui/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts) \
-    vendor/halcyonui/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
-
-PRODUCT_PACKAGES += \
-    FontArbutus \
-    FontArvoLato \
-    FontGoogleSans \
-    FontKai \
-    FontManrope \
-    FontNotoSerifSource \
-    FontOnePlusSans \
-    FontRubik \
-    FontVictor
-
 # Overlays
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/halcyonui/overlay
 DEVICE_PACKAGE_OVERLAYS += \
     vendor/halcyonui/overlay/common/
-
-# Icon Packs
-PRODUCT_PACKAGES += \
-    IconPackCircularAndroidOverlay \
-    IconPackCircularLauncherOverlay \
-    IconPackCircularSettingsOverlay \
-    IconPackCircularSystemUIOverlay \
-    IconPackCircularThemePickerOverlay \
-    IconPackVictorAndroidOverlay \
-    IconPackVictorLauncherOverlay \
-    IconPackVictorSettingsOverlay \
-    IconPackVictorSystemUIOverlay \
-    IconPackVictorThemePickerOverlay \
-    IconPackSamAndroidOverlay \
-    IconPackSamLauncherOverlay \
-    IconPackSamSettingsOverlay \
-    IconPackSamSystemUIOverlay \
-    IconPackSamThemePickerOverlay \
-    IconPackKaiAndroidOverlay \
-    IconPackKaiLauncherOverlay \
-    IconPackKaiSettingsOverlay \
-    IconPackKaiSystemUIOverlay \
-    IconPackKaiThemePickerOverlay \
-    IconPackFilledAndroidOverlay \
-    IconPackFilledLauncherOverlay \
-    IconPackFilledSettingsOverlay \
-    IconPackFilledSystemUIOverlay \
-    IconPackFilledThemePickerOverlay \
-    IconPackRoundedAndroidOverlay \
-    IconPackRoundedLauncherOverlay \
-    IconPackRoundedSettingsOverlay \
-    IconPackRoundedSystemUIOverlay \
-    IconPackRoundedThemePickerOverlay \
-    IconPackOxygenAndroidOverlay \
-    IconPackOxygenLauncherOverlay \
-    IconPackOxygenSettingsOverlay \
-    IconPackOxygenSystemUIOverlay \
-    IconPackOxygenThemePickerOverlay \
-    IconPackOutlineAndroidOverlay \
-    IconPackOutlineSettingsOverlay \
-    IconPackOutlineSystemUIOverlay
-
-PRODUCT_PACKAGES += \
-    IconShapeRoundedRectOverlay \
-    IconShapeSquareOverlay \
-    IconShapeSquircleOverlay \
-    IconShapeTeardropOverlay
 
 # Power off charing resource
 PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
-# Quick Tap
-ifeq ($(TARGET_SUPPORTS_QUICK_TAP),true)
-PRODUCT_PACKAGES += \
-    quick_tap
-endif
-
 # Nest Launcher
 PRODUCT_PACKAGES += \
-    NestQuickStep \
     ThemedIconsOverlay
 
-# Theme
-PRODUCT_PACKAGES += \
-    HalcyonBlackTheme
 
-# Theme Picker
 PRODUCT_PACKAGES += \
     ThemePicker
-
-# Google Dialer Config
-PRODUCT_PACKAGES += \
-    GoogleDialerConfOverlay
