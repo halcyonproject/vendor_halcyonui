54d38afc8ab39ed634ab55bda54445fee92116e6
HeroBuxx <me@herobuxx.me>
2024-12-14 17:26:34 +0530

    halcyonUIL bootanimation: Shut up
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -5,15 +5,8 @@
 #
 
 # Bootanimation
-ifeq ($(TARGET_SCREEN_WIDTH),)
-     $(warning "TARGET_SCREEN_WIDTH is undefined, assuming 1080p")
-     TARGET_SCREEN_WIDTH ?= 1080
-     TARGET_SCREEN_HEIGHT ?= 1920
-else
-     $(warning "Current TARGET_SCREEN_WIDTH res is not supported, forcing 1080p")
-     TARGET_SCREEN_WIDTH ?= 1080
-     TARGET_SCREEN_HEIGHT ?= 1920
-endif
+TARGET_SCREEN_WIDTH ?= 1080
+TARGET_SCREEN_HEIGHT ?= 1920
 
 # Bootanimation
 PRODUCT_PACKAGES += \
