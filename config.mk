25b462fcbcda5372ea1d734c70497a79254c4702
HeroBuxx <me@herobuxx.me>
2025-03-14 10:03:42 +0800

    halcyonUI: Bring back SystemUIClocks
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- config.mk
+++ config.mk
@@ -88,6 +88,17 @@ PRODUCT_PACKAGES += \
     IconShapeSquircleOverlay \
     IconShapeTeardropOverlay
 
+# Lockscreen Clock Styles
+PRODUCT_PACKAGES += \
+     SystemUIClocks-BigNum \
+     SystemUIClocks-Calligraphy \
+     SystemUIClocks-Flex \
+     SystemUIClocks-Growth \
+     SystemUIClocks-Inflate \
+     SystemUIClocks-Metro \
+     SystemUIClocks-NumOverlap \
+     SystemUIClocks-Weather
+     
 # Navigation Bar
 PRODUCT_PACKAGES += \
     ImmersiveNavigationOverlay
